2d3086d0d9f540f0b9965c5b5b43bcc6c55956ff
aymeric <aymeric@b4fb6cef-10e1-4022-a11b-4669fa468031>
2003-07-31 10:11:34 +0000

    fix copyrigth
    git-svn-id: svn://svn.savannah.nongnu.org/exosip/trunk/exosip@181 b4fb6cef-10e1-4022-a11b-4669fa468031

--- josua/jcalls.h
+++ josua/jcalls.h
@@ -1,10 +1,21 @@
 /*
- * sh_phone - Shell Phone
+ * josua - Jack's open sip User Agent
  *
  * Copyright (C) 2002,2003   Aymeric Moizard <jack@atosc.org>
  *
- * This software is protected by copyright. For more information
- * write to <jack@atosc.org>.
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with dpkg; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef __JCALLS_H__
