aed5ef4ca6d4613b1cdd6b9ef7b75c3705ba3d32
aymeric <aymeric@b4fb6cef-10e1-4022-a11b-4669fa468031>
2005-12-11 19:33:12 +0000

    fix for bsd
    git-svn-id: svn://svn.savannah.nongnu.org/exosip/trunk/exosip@634 b4fb6cef-10e1-4022-a11b-4669fa468031

--- tools/sip_reg.c
+++ tools/sip_reg.c
@@ -28,6 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <syslog.h>
 #include <pthread.h>
